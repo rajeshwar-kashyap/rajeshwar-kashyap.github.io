cb0b99f38ede368f8b8abe5cae2263dc3d44e8f3
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-09-06 22:31:16 +0530

    add footer logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,11 +7,11 @@ export default function Footer() {
 		<div className='mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 py-3'>
 			<div className='flex flex-col items-center justify-center'>
 				<div>
-					{theme === 'light' ? (
+					{theme === 'dark' ? (
 						// eslint-disable-next-line @next/next/no-img-element
 						<img
 							src={
-								'https://ik.imagekit.io/rjkashyap05/portfolio/logo-light.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1662305259583'
+								'https://ik.imagekit.io/rjkashyap05/portfolio/logo-dark.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1662305259583'
 							}
 							className='w-[100px]  inline'
 							alt='Logo'
@@ -20,7 +20,7 @@ export default function Footer() {
 						// eslint-disable-next-line @next/next/no-img-element
 						<img
 							src={
-								'https://ik.imagekit.io/rjkashyap05/portfolio/logo-dark.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1662305285290'
+								'https://ik.imagekit.io/rjkashyap05/portfolio/logo-light.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1662305285290'
 							}
 							className='w-[100px]  inline'
 							alt='Logo'
