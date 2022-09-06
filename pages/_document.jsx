10be303e4b0e8833c72aecfd77547c297f001d79
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-09-06 22:50:12 +0530

    add google tag

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,18 @@ class MyDocument extends Document {
 					/>
 					<link rel='icon' href='/favicon.png' />
 					<html lang='en' />
+					<Script id='google-analytics' strategy='afterInteractive'>
+						{`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PKWH4TCGB5');
+        `}
+					</Script>
+					<Script
+						src='https://www.googletagmanager.com/gtag/js?id=G-PKWH4TCGB5'
+						strategy='afterInteractive'
+					/>
 				</Head>
 				<body>
 					<Main />
